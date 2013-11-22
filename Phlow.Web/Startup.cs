f41faec8d7cf7c3d22c2b869c8bfcfc43ecb8e8d
anurse <andrew@andrewnurse.net>
2013-11-21 16:27:21 -0800

    Initial Scaffold

---
+++ Phlow.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace Phlow.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.Run(async ctx =>
+            //{
+            //    await ctx.Response.WriteAsync("Foo");
+            //});
+        }
+    }
+}
